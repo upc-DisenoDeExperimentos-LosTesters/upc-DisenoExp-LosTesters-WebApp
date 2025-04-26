96f9eb78702785b0144a67d951a8798fb821e966
Fasz0711 <fabrizio0711@hotmail.com>
2025-04-26 17:49:51 -0500

    fix: update Axios instance base URL

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -5,8 +5,8 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 * @type {axios. AxiosInstance}
 * */
 const http = axios.create({
-    baseURL : "https://my-json-server.typicode.com/upc-AppWeb-BicasTeam",
-    headers:{'Content-type':'application/json'}
+  baseURL: "https://lostesters-app-service-08e7da98ea3f.herokuapp.com/",
+  headers: { "Content-type": "application/json" },
 });
 
 export default http;
