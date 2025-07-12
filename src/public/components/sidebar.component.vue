2038c8cd4bc2339f2f4ff51c5895bff00e0a189c
JDu202012207 <u202012207@upc.edu.pe>
2025-07-12 18:24:20 -0500

    feat(sidebar): add language toggle functionality and update sidebar labels for localization

--- src/public/components/sidebar.component.vue
+++ src/public/components/sidebar.component.vue
@@ -33,6 +33,11 @@ export default {
       this.isMobile = window.innerWidth <= 860;
       this.sidebarOpen = !this.isMobile;
     },
+    toggleLanguage() {
+      const newLocale = this.$i18n.locale === 'es' ? 'en' : 'es';
+      this.$i18n.locale = newLocale;
+      localStorage.setItem('lang', newLocale);
+    },
     toggleSidebar() {
       this.sidebarOpen = !this.sidebarOpen;
     },
@@ -112,41 +117,43 @@ export default {
             <li @click="goToHome">
               <a v-ripple class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple">
                 <i class="pi pi-home mr-2 text-xl"></i>
-                <span class="font-medium text-xl">Home</span>
+                <span class="font-medium text-xl">{{ $t('sidebar.home') }}</span>
               </a>
             </li>
             <li @click="goToOrganizations">
               <a v-ripple class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple">
                 <i class="pi pi-users mr-2 text-xl"></i>
-                <span class="font-medium text-xl">Organization</span>
+                <span class="font-medium text-xl">{{ $t('sidebar.shipments') }}</span>
               </a>
             </li>
             <li @click="goToReports">
               <a v-ripple class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple">
                 <i class="pi pi-chart-bar mr-2 text-xl"></i>
-                <span class="font-medium text-xl">Reports</span>
+                <span class="font-medium text-xl">{{ $t('sidebar.reports') }}</span>
               </a>
             </li>
             <li @click="goToVehicles">
               <a v-ripple class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple">
                 <i class="pi pi-truck mr-2 text-xl"></i>
-                <span class="font-medium text-xl">Vehicles</span>
+                <span class="font-medium text-xl">{{ $t('sidebar.vehicles') }}</span>
               </a>
             </li>
           </ul>
         </div>
         <div>
           <ul class="list-none p-3 m-0">
-            <li @click="goToProfile">
-              <a v-ripple class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple">
-                <i class="pi pi-cog mr-2 text-xl"></i>
-                <span class="font-medium text-xl">Settings</span>
+            <li @click="toggleLanguage">
+              <a class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple">
+                <i class="pi pi-globe mr-2 text-xl"></i>
+                <span class="font-medium text-xl">
+                {{ $i18n.locale === 'es' ? 'EN' : 'ES' }}
+              </span>
               </a>
             </li>
             <li @click="logout">
               <a v-ripple class="flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors p-ripple">
                 <i class="pi pi-sign-out mr-2 text-xl"></i>
-                <span class="font-medium text-xl">Logout</span>
+                <span class="font-medium text-xl">{{ $t('sidebar.logout') }}</span>
               </a>
             </li>
           </ul>
